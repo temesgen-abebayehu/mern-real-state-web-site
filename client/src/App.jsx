93e65c01caae162b0b559d9691c1d5714734d3fd
temesgen-abebayehu <temesgensoftware21@gmail.com>
2024-10-06 10:45:10 +0300

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,20 @@
-function App() {
+import {Route, Routes}  from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import ProfilePage from './pages/ProfilePage';
 
+function App() {
   return (
     <>
-      <h1 className="text-red-900 font-bold underline">Hello world!</h1>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/about' element={<AboutPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
+        <Route path='/profile' element={<ProfilePage />} />
+      </Routes>
     </>
   );
 }
