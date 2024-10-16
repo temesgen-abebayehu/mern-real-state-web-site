2a6f40ade40cb67db6c7f9953c50203a89136fe5
temesgen-abebayehu <temesgensoftware21@gmail.com>
2024-10-16 21:20:58 +0300

    add redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,18 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { store } from './redux/store'
+import { persister, store } from './redux/store'
 import { Provider } from 'react-redux'
 
 import App from "./App.jsx";
 import "./index.css";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persister}>
+        <App />
+      </PersistGate>
     </Provider>
     ,
   </BrowserRouter>
