7130f926a6079dae2f9d78524c6a3a7a92e4cebe
temesgen-abebayehu <temesgensoftware21@gmail.com>
2024-10-18 15:58:20 +0300

    complete profile page UI

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+
 
 function ProfilePage() {
   return (
-    <div>ProfilePage</div>
-  )
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center p-7">Profile</h1>
+      <form className="flex flex-col gap-3">
+        <input
+          type="text"
+          placeholder="Username"
+          id="username"
+          className="p-3 border rounded-md focus:outline-none"
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          className="p-3 border rounded-md focus:outline-none"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="p-3 border rounded-md focus:outline-none"
+        />
+        <button
+          type="submit"
+          className="bg-emerald-600 text-white rounded-md uppercase p-3 font-semibold hover:opacity-90 disabled:opacity-75"
+        >
+          Update Profile
+        </button>
+      </form>
+
+      <div className="flex justify-between my-4">
+        <span className="text-red-600 cursor-pointer font-semibold">Delete accoute</span>
+        <span className="text-red-600 cursor-pointer font-semibold">Logout</span>
+      </div>
+    </div>
+  );
 }
 
-export default ProfilePage
+export default ProfilePage;
