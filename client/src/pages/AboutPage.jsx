eadb1854c311959605b96f75b7c48d8d00ea0c19
temesgen-abebayehu <temesgensoftware21@gmail.com>
2024-11-07 20:55:03 +0300

    update title to "Tom Estate" and enhance AboutPage with detailed content

--- client/src/pages/AboutPage.jsx
+++ client/src/pages/AboutPage.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
 
 function AboutPage() {
   return (
-    <div>AboutPage</div>
-  )
+    <div className="flex flex-col gap-4 p-12">
+      <h1 className="text-slate-700 font-bold text-3xl mb-3">Tom Real Estate</h1>
+      <p className="">
+        Tom Estate is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged.
+      </p>
+      <p>
+        Our mission It was popularised in the 1960s with the release of Letraset sheets
+        containing Lorem Ipsum passages, and more recently with desktop
+        publishing software like Aldus PageMaker including versions of Lorem
+        Ipsum.
+      </p>
+      <p>
+        Our team It is a long established fact that a reader will be distracted by the
+        readable content of a page when looking at its layout. The point of
+        using Lorem Ipsum is that it has a more-or-less normal distribution of
+        letters, as opposed to using 'Content here, content here', making it
+        look like readable English.
+      </p>
+    </div>
+  );
 }
 
-export default AboutPage
+export default AboutPage;
