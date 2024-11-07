3a614d55fb005f0b1269a659a46004c41c81ec3d
temesgen-abebayehu <temesgensoftware21@gmail.com>
2024-11-07 21:08:35 +0300

    preparation to deployment

--- api/index.js
+++ api/index.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
 
 import connectMongoDB from './db/connectMongoDB.js';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
+
+const __dirname = path.resolve();
 
 dotenv.config();
 const app = express();
@@ -21,6 +24,12 @@ const PORT = process.env.PORT || 3000;
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err, req, res, next)=>{
     const statusCode = err.statusCode || 500;
